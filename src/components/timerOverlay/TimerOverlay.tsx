0b2f5debf6573adb0eef531037e33152453491ec
Ivan <ereyzer142@gmail.com>
2025-10-04 18:36:36 +0300

    init dev

---
+++ src/components/timerOverlay/TimerOverlay.tsx
@@ -0,0 +1,10 @@
+import "./timerOverlay.css";
+function TimerOverlay() {
+  return (
+    <div className="timer-overlay-container">
+      <p>Text</p>
+    </div>
+  );
+}
+
+export default TimerOverlay;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./css/colors.css";
+import App from "./App.tsx";
+import TimerOverlay from "./components/timerOverlay/TimerOverlay.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+createRoot(document.getElementById("timer-overlay-root")!).render(
+  <StrictMode>
+    <TimerOverlay />
+  </StrictMode>
+);
