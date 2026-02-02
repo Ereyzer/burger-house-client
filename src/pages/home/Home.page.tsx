80641eaa5ba1f27abf43c497ce572b5435e2d9d5
Ivan <ereyzer142@gmail.com>
2026-02-02 19:34:52 +0200

    fix seo problems

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -206,10 +206,10 @@ function HomePage() {
         <ModalCardContextProvider>
           <ul
             className={clsx(css.productList)}
+            role="list"
             aria-labelledby={`category-tab-${activCategory}`}
             aria-label="Список страв. Кожна картка містить назву, фото і кнопку для додавання в кошик."
             id={`category-panel-${activCategory}`}
-            role="tabpanel"
             tabIndex={0}
           >
             {products.items.map(product => (
